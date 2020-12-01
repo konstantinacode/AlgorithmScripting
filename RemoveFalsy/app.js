2bac442f6ecdf8534e1d0c0380516c0e3ff41187
konstantinacode <konstantina_mat@hotmail.com>
2020-12-01 21:55:06 +0200

    Remove all falsy values from an array

---
+++ RemoveFalsy/app.js
@@ -0,0 +1,17 @@
+/*
+Remove all falsy values from an array.
+
+Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+Hint: Try converting each value to a Boolean.
+*/
+function bouncer(arr) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]) newArray.push(arr[i]);
+    }
+    console.log(newArray);
+    return newArray;
+}
+
+bouncer([false, null, 0, NaN, undefined, ""]);
